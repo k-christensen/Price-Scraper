9d6857a7c4cc9949c559a9015b74527d19b5a44a
Kate Christensen <katec125@gmail.com>
2020-09-02 03:00:18 -0500

    added git ignore for password

--- price_scraper.py
+++ price_scraper.py
@@ -1,16 +1,23 @@
 import smtplib
 
+from smtplib import SMTPException
+
 sender = 'katec125@gmail.com'
 receivers = ['katec125@gmail.com']
 
 message = """
+From: From Person <katec125@gmail.com>
+To: To Person <katec125@gmail.com>
+Subject: SMTP e-mail test
 Here is where the sample text would be
 """
-
 try:
-   smtpObj = smtplib.SMTP('localhost')
-   smtpObj.sendmail(sender, receivers, message)
-   print("Successfully sent email")
-
+    session = smtplib.SMTP('smtp.gmail.com', 587)
+    session.ehlo()
+    session.starttls()
+    session.ehlo()
+    session.login(sender, 'password')
+    session.sendmail(sender, receivers, message)
+    session.quit()
 except SMTPException:
-   print("Error: unable to send email")
+    print('Error')
